f59b6b613e2253585a61977b817e6c4bd5216102
Paul Hendricks <paul.hendricks.2013@owu.edu>
2015-11-23 11:47:33 -0500

    updated munge

--- munge/01-train.py
+++ munge/01-train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-train = pd.read_csv("../data/original/train.csv")
+train = pd.read_csv("../data/original/train.csv", index_col=0)
 
 train.to_csv("../data/prepped/train.csv")
 __author__ = 'paul'
--- munge/02-test.py
+++ munge/02-test.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 
-train = pd.read_csv("../data/original/test.csv")
+train = pd.read_csv("../data/original/test.csv", index_col=0)
 
 train.to_csv("../data/prepped/test.csv")
